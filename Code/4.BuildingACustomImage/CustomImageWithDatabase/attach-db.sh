d2b6fd4c703d8eae779fa2b081c27a7055c02bb3
Andrew Pruski <andrew.pruski@channeladvisor.com>
2020-11-24 09:36:14 +0000

    updated notebooks

--- Code/4.BuildingACustomImage/CustomImageWithDatabase/attach-db.sh
+++ Code/4.BuildingACustomImage/CustomImageWithDatabase/attach-db.sh
@@ -1,4 +1,4 @@
-sleep 15s
+sleep 30s
  
 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P Testing1122 \
--Q "CREATE DATABASE [testdatabase] ON (FILENAME = '/var/opt/sqlserver/data/testdatabase.mdf'),(FILENAME = '/var/opt/sqlserver/log/testdatabase_log.ldf') FOR ATTACH"
+-Q "CREATE DATABASE [testdatabase] ON (FILENAME = '/var/opt/sqlserver/testdatabase.mdf'),(FILENAME = '/var/opt/sqlserver/testdatabase_log.ldf') FOR ATTACH"
